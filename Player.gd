17fddf56372bfbd27d80f66053aad4fa9f373535
Dornagol <feufeuhugo@gmail.com>
2024-11-15 15:31:57 +0100

    oui

--- Player.gd
+++ Player.gd
@@ -1,36 +1,65 @@
 extends CharacterBody3D
 
+signal health_changed(health_value)
+
 @onready var camera = $Camera3D
 @onready var anim_player = $AnimationPlayer
+@onready var muzzle_flash = $Camera3D/Pistol/MuzzleFlash
+@onready var raycast = $Camera3D/RayCast3D
+@onready var shootsound = $ShootSound
 
+var health = 3
 const SPEED = 10.0
 const JUMP_VELOCITY = 10.0
+const SPAWN_POSITIONS = [
+	Vector3.ZERO,
+	Vector3(0, 0, 5),
+	Vector3(5, 0, 0),
+	Vector3(-5, 0, 0),
+	Vector3(0, 0, -5)
+]
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = 20.0
 
+func _enter_tree():
+	set_multiplayer_authority(str(name).to_int())
+	# Set initial spawn position based on player ID
+	var player_id = str(name).to_int()
+	position = SPAWN_POSITIONS[player_id % SPAWN_POSITIONS.size()]
+
 func _ready():
+	if not is_multiplayer_authority(): return
+	
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	camera.current = true
+	
+	for child in $WorldModel.find_children("*", "VisualInstance3D"):
+		child.set_layer_mask_value(1, false)
+		child.set_layer_mask_value(2, false)
 
 func _unhandled_input(event):
+	if not is_multiplayer_authority(): return
 	if event is InputEventMouseMotion:
 		rotate_y(-event.relative.x * .005)
 		camera.rotate_x(-event.relative.y * .005)
 		camera.rotation.x = clamp(camera.rotation.x, -PI/2, PI/2)
 		
-	if Input.is_action_just_pressed("shoot") and anim_player.current_animation != "shoot":
-		play_shoot_effects()
-		
+	if Input.is_action_just_pressed("shoot") \
+			and anim_player.current_animation != "shoot":
+		play_shoot_effects.rpc()
+		if raycast.is_colliding():
+			var hit_player = raycast.get_collider()
+			hit_player.receive_damage.rpc_id(hit_player.get_multiplayer_authority())
 
 func _physics_process(delta):
+	if not is_multiplayer_authority(): return
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
-
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("left", "right", "up", "down")
@@ -41,16 +70,44 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-
 	if anim_player.current_animation == "shoot":
 		pass
 	elif input_dir != Vector2.ZERO and is_on_floor():
 		anim_player.play("move")
 	else:
 		anim_player.play("idle")
+	if is_multiplayer_authority():
+   	# Votre code de mouvement existant...
+		move_and_slide()
+	# Synchroniser la position avec les autres clients
+	sync_position.rpc(position, rotation)
 
-	move_and_slide()
-
+@rpc("call_local")
 func play_shoot_effects():
 	anim_player.stop()
 	anim_player.play("shoot")
+	shootsound.play()
+	muzzle_flash.restart()
+	muzzle_flash.emitting = true
+
+@rpc("any_peer")
+func receive_damage():
+	health -= 1
+	health_changed.emit(health)  # Émettre le signal avant de vérifier la mort
+	if health <= 0:
+		health = 3
+		# Utiliser la même logique de spawn que dans _enter_tree
+		var player_id = str(name).to_int()
+		position = SPAWN_POSITIONS[player_id % SPAWN_POSITIONS.size()]
+		health_changed.emit(health)  # Émettre le signal après la réinitialisation
+
+func _on_animation_player_animation_finished(anim_name):
+	if anim_name == "shoot":
+		anim_player.play("idle")
+
+# Dans Player.gd, ajoutez ceci
+@rpc("unreliable", "any_peer", "call_local")
+func sync_position(pos: Vector3, rot: Vector3):
+	if not is_multiplayer_authority():
+		position = pos
+		rotation = rot
--- world.gd
+++ world.gd
@@ -1,5 +1,74 @@
 extends Node
 
+@onready var main_menu = $CanvasLayer/MainMenu
+@onready var address_entry = $CanvasLayer/MainMenu/MarginContainer/VBoxContainer/AddressEntry
+@onready var hud = $CanvasLayer/HUD
+@onready var health_bar = $CanvasLayer/HUD/HealthBar
+@onready var igmusic = $IngameMusic
+@onready var menumusic = $MenuMusic
+@onready var bg = $CanvasLayer/background
+
+const Player = preload("res://scene/player.tscn")
+const PORT = 9999
+
+var enet_peer = ENetMultiplayerPeer.new()
+var players = {}  # Pour suivre les joueurs connectés
+
+func _ready():
+	menumusic.play()
+
 func _unhandled_input(event):
 	if Input.is_action_just_pressed("quit"):
 		get_tree().quit()
+
+func _on_host_button_pressed():
+	main_menu.hide()
+	bg.hide()
+	menumusic.stop()
+	igmusic.play()
+	hud.show()
+	
+	# Configuration du serveur
+	enet_peer.create_server(PORT)
+	multiplayer.multiplayer_peer = enet_peer
+	multiplayer.peer_connected.connect(add_player)
+	multiplayer.peer_disconnected.connect(remove_player)
+	
+	# Ajouter l'hôte en tant que premier joueur
+	add_player(1)  # L'hôte a toujours l'ID 1
+
+func _on_join_button_pressed():
+	main_menu.hide()
+	bg.hide()
+	menumusic.stop()
+	igmusic.play()
+	hud.show()
+	
+	# Configuration du client
+	enet_peer.create_client("localhost", PORT)
+	multiplayer.multiplayer_peer = enet_peer
+
+func add_player(peer_id):
+	# Vérifier si le joueur n'existe pas déjà
+	if not players.has(peer_id):
+		var player = Player.instantiate()
+		player.name = str(peer_id)
+		add_child(player)
+		players[peer_id] = player
+		
+		# Connecter le signal de santé seulement pour le joueur local
+		if peer_id == multiplayer.get_unique_id():
+			player.health_changed.connect(update_health_bar)
+
+func remove_player(peer_id):
+	if players.has(peer_id):
+		if players[peer_id]:
+			players[peer_id].queue_free()
+		players.erase(peer_id)
+
+func update_health_bar(health_value):
+	health_bar.value = health_value
+
+func _on_multiplayer_spawner_spawned(node):
+	if node.is_multiplayer_authority():
+		node.health_changed.connect(update_health_bar)
